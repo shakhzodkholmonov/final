686738aa945fbc6011363f151be9526495d35e9a
Shakhzod <shakhzodkholmononov@gmail.com>
2024-03-16 10:24:09 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,394 @@
+let product = [
+    {
+        id: Math.random(),
+        name: "Oshxona anjomlari to'plami Kukmara, Granit Ultra liniyasi AP",
+        description: "5.0 (43 sharh)",
+        price: "47 880 so'm/oyiga",
+        realprice: "598 000 so'm",
+        sale: "399 000 so'm",
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cnemuk125kub33f5f2m0/t_product_540_high.jpg#1709015652203"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Ofis texnikalari uchun qog'oz varaqlari Sylvamo Svetotopy, A4f C07/3, 500 varaq",
+        description: "5.0 (2624 sharh)",
+        price: "5 180 so'm/oyiga",
+        realprice: "57 000 so'm",
+        sale: "43 000 so'm",
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/ck1descvutvccfo27mn0/t_product_540_high.jpg#1709015652203"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Qahva Carte Noire, sublimatsiyalangan, 95 g",
+        description: "5.0 (146 sharh)",
+        price: "6 000 so'm/oyiga",
+        realprice: "85 000 so'm",
+        sale: "55 000 so'm",
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/ccv6he35a95unf121g5g/t_product_540_high.jpg#1709015652203"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Tikuv mashinasi Janome HomeDecor 2077",
+        description: "5.0 (33 sharh)",
+        price: "226 800 so'm/oyiga",
+        realprice: "2 850 000 so'm",
+        sale: "1 890 000 so'm",
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cc8egs6ha881nt1ejt40/t_product_540_high.jpg#1709015652204"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Batareykalar Duracell SIMPLY AA 2X10CRD MON, 2 dona",
+        description: "5.0 (21 sharh)",
+        price: "1 440 so'm/oyiga",
+        realprice: "20 000 so'm",
+        sale: "12 000 so'm",
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/ckie8okjvf2sulnv3an0/t_product_540_high.jpg#1709015652204"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Oshxona anjomlari to'plami Kukmara № 8 Mramornaya, 3 dona",
+        description: "4.9 (137 sharh)",
+        price: "57 720 so'm/oyiga",
+        realprice: "80 000 so'm",
+        sale: "481 000 so'm",
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cf0prk0l08k0o9qho880/t_product_540_high.jpg#1709015652204"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "O'g'il bolalar uchun kombinezon-ishton, velyur",
+        description: "5.0 (7 sharh)",
+        price: "7 800 so'm/oyiga",
+        realprice: "98 000 so'm",
+        sale: "65 000 so'm",
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/ce6u94gv1htd23aiovcg/t_product_540_high.jpg#1709015652204"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Dezinfektsiya qiluvchi vosita Cillit Bang Dog'ga qarshi + jilo",
+        description: "4.9 (148 sharh)",
+        price: "3 480 so'm/oyiga",
+        realprice: "43 000 so'm",
+        sale: "29 000 so'm",
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cccd1reha8813vtohgtg/t_product_540_high.jpg#1709015652205"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Shampun L'Oreal Paris Elseve Gialuron to'ldiruvchisi, suvsizlangan va yupqa sochlar uchun, 250 ml",
+        description: "4.9 (9 sharh)",
+        price: "3 224 so'm/oyiga",
+        realprice: "38 000 so'm",
+        sale: "27 000 so'm",
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cc8siruha88139ljbj50/t_product_540_high.jpg#1709186648887"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Soch uchun bo'yog'i L'Oreal Paris Excellence Cool Creme",
+        description: "4.9 (71 sharh)",
+        price: "7 560 so'm/oyiga",
+        realprice: "76 000 so'm",
+        sale: "63 000 so'm",
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cdkv9bv0tgqvlm59f1pg/t_product_540_high.jpg#1709015652205"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Tashkilotchi, quti, shkafning tanasi, narsalar va o'yinchoqlarni saqlash uchun idish",
+        description: "4.7 (355 sharh)",
+        price: "8 400 so'm/oyiga",
+        realprice: "130 000 so'm",
+        sale: "70 000 so'm",
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/ccq827n0tgqvlm57l8h0/t_product_540_high.jpg#1709186810897"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Masofadan boshqariladigan, suv o'tkazmaydigan amfibiya mashinasi, boshqaruv pult va braslet",
+        description: "5.0 (358 sharh)",
+        price: "37 800 so'm/oyiga",
+        realprice: "450 000 so'm",
+        sale: "315 000 so'm",
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/clrcm19iac3an8rm5080/t_product_540_high.jpg#1709186914897"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Konsiler yuz uchun, tonal tuzatuvchi, 3.5 g",
+        description: "4.9 (118 sharh)",
+        price: "1 200 so'm/oyiga",
+        realprice: "100 000 so'm",
+        sale: "10 000 so'm",
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/chod91d6sfhndlbmvp1g/t_product_540_high.jpg#1709187046867"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Blinchik qovuradigan tova tutqich bilan Kukmara Trendy Style, malachite",
+        description: "4.8 (229 sharh)",
+        price: "18 720 so'm/oyiga",
+        realprice: "180 000 so'm",
+        sale: "156 000 so'm",
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cbvmivcff5b95v3joc0g/original.jpg"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Ayyoolar uchun jemper Lamita",
+        description: "4.9 (103 sharh)",
+        price: "16 320 so'm/oyiga",
+        realprice: "210 000 so'm",
+        sale: "136 000 so'm",
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/clondnvn7c6gg9ie418g/t_product_540_high.jpg#1709187290139"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Oshxona elektron tarozisi SF-400, 10 kg gacha",
+        description: "5.0 (404 sharh)",
+        price: "6 840 so'm/oyiga",
+        realprice: "150 000 so'm",
+        sale: "65 000 so'm",
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cd348ej5a95unf1268cg/original.jpg"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Radio boshqariladigan o'yinchoq, bolalar uchun mini dron, qo'l bilan boshqariladiga",
+        description: "4.9 (81 sharh)",
+        price: "8 280 so'm/oyiga",
+        realprice: "148 000 so'm",
+        sale: "79 000 so'm",
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/ceac7qgl08kcldtokfg0/original.jpg"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Shishali choynak, bambuk qopqoqli va filtr prujinali",
+        description: "4.8 (175 sharh)",
+        price: "5 040 so'm/oyiga",
+        realprice: "190 000 so'm",
+        sale: "42 000 so'm",
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cler0lnn7c6qm23jtp30/t_product_540_high.jpg#1709187579984"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Quyultirilgan sut Alekseevskoe, 8.5%, 360 g",
+        description: "4.9 (38 sharh)",
+        price: "3 960 so'm/oyiga",
+        realprice: "34 000 so'm",
+        sale: "33 000 so'm",
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cn0bsvhs99ouqbft99g0/t_product_540_high.jpg#1709187693368"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Islomiy erkaklar soati Al Harameen HA-6506",
+        description: "4.8 (426 sharh)",
+        price: "40 560 so'm/oyiga",
+        realprice: "450 000 so'm",
+        sale: "338 000 so'm",
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cli6fdtennt1kt4dv79g/t_product_540_high.jpg#1709187784320"],
+        category: 1,
+    },
+];
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+const shuffledProduct = shuffle(product);
+
+const container = document.querySelector(".grid");
+const btn = document.getElementById("btn")
+
+let productDisplayed = 5;
+
+showProducts(5);
+productDisplayed = 5;
+btn.textContent = "Yana ko'rsatish 20";
+
+function toggleProducts() {
+    if(productDisplayed === 0) {
+        showProducts(5);
+        productDisplayed = 5;
+       btn.textContent =  "Yana ko'rsatish 20";
+    } else if (productDisplayed === product.length) {
+        hideProducts(0);
+        productDisplayed = 5;
+        btn.textContent =  "Show Products";
+    } else {
+        const remainingProducts = product.length - productDisplayed;
+        const productsToShow = Math.min(5, remainingProducts);
+        productDisplayed += productsToShow;
+        showProducts(productsToShow);
+        if (productDisplayed === product.length) {
+            toggleButton.textContent =  "Yana ko'rsatish 20";
+        }
+    }
+}
+
+function showProducts(count) {
+    const productsToShow = product.slice(productDisplayed, productDisplayed + count);
+    addProductsToContainer(productsToShow);
+}
+
+function hideProducts() {
+    // Hide all cards except the first 5
+    const cards = container.querySelectorAll('.card');
+    for (let i = 5; i < cards.length; i++) {
+        cards[i].style.display = 'none';
+    }
+}
+
+function addProductsToContainer(products) {
+    products.forEach((product) => {
+        const card = document.createElement("div");
+        card.classList.add("card", "bg-white", "rounded-lg", "overflow-hidden", "hover:shadow-md");
+    
+        const images = product.images
+            .map((image) => `<img src='${image}' alt='${product.name}' class='w-full h-84 object-cover rounded-lg'/>`)
+            .join("");
+    
+        card.innerHTML = `
+          <div class='overflow-hidden p-2'>
+            <div class="absolute">
+              <h1 class="text-white bg-red-500 rounded-[5px] w-[50px] h-[22px] pt-[1px] pl-[6px] mt-[296px] ml-[1px]">Savdo</h1>
+            </div>
+           <div>${images}
+           <label class="like-container">
+        <input type="checkbox">
+        <div class="checkmark">
+          <svg viewBox="0 0 256 256">
+          <rect fill="none" height="256" width="256"></rect>
+          <path d="M224.6,51.9a59.5,59.5,0,0,0-43-19.9,60.5,60.5,0,0,0-44,17.6L128,59.1l-7.5-7.4C97.2,28.3,59.2,26.3,35.9,47.4a59.9,59.9,0,0,0-2.3,87l83.1,83.1a15.9,15.9,0,0,0,22.6,0l81-81C243.7,113.2,245.6,75.2,224.6,51.9Z" stroke-width="20px" stroke="#FFF" fill="none"></path></svg>
+        </div>
+      </label></div>
+           <h2 class="text-sm text-gray-700">${product.name}</h2>
+           <h2 class="text-xs text-gray-500"><span class="fa fa-star checked"></span>${product.description}</h2>
+           <p class="price mt-2 w-32">${product.price}</p>
+           <p class="mt-6 line-through text-xs text-slate-400">${product.realprice}</p>
+           <p>${product.sale}</p>
+                  <div class="border h-8 w-8 rounded-full flex center
+           items-center ml-52">
+        <div data-v-1a3a46a8="" class="slot default"><svg data-v-40da8b10="" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="ui-icon  add-cart-icon">
+      <path d="M8 10V8H6V12.5C6 12.7761 5.77614 13 5.5 13C5.22386 13 5 12.7761 5 12.5V7H8C8 4.59628 9.95227 3 12 3C14.0575 3 16 4.70556 16 7H19V19.5C19 20.3284 18.3284 21 17.5 21H12.5C12.2239 21 12 20.7761 12 20.5C12 20.2239 12.2239 20 12.5 20H17.5C17.7761 20 18 19.7761 18 19.5V8H16V10H15V8H9V10H8ZM12 4C10.4477 4 9 5.20372 9 7H15C15 5.29444 13.5425 4 12 4Z" fill="black"></path>
+      <path d="M7.5 14C7.77614 14 8 14.2239 8 14.5V17H10.5C10.7761 17 11 17.2239 11 17.5C11 17.7761 10.7761 18 10.5 18H8V20.5C8 20.7761 7.77614 21 7.5 21C7.22386 21 7 20.7761 7 20.5V18H4.5C4.22386 18 4 17.7761 4 17.5C4 17.2239 4.22386 17 4.5 17H7V14.5C7 14.2239 7.22386 14 7.5 14Z" fill="black"></path>
+      </svg></div>
+      </div>
+          </div>
+         `;
+    
+        container.appendChild(card);
+    });
+}
+
+btn.addEventListener("click", toggleProducts);
+
+// Button to open modal
+document.getElementById("openModalButton").addEventListener("click", () => {
+    document.getElementById("modal").classList.remove("hidden");
+});
+
+// Button to close modal
+document.getElementById("closeModalButton").addEventListener("click", () => {
+    document.getElementById("modal").classList.add("hidden");
+});
+
+// Search Products
+const searchInput = document.getElementById("searchInput");
+const searchResults = document.getElementById("searchResults");
+
+function displayProducts(products) {
+  searchInput.innerHTML = "";
+
+  if (product.length === 0) {
+    const noResultsMessage = document.createElement("p");
+    noResultsMessage.textContent = "No Product";
+    noResultsMessage.classList.add("text-lg");
+    searchResults.appendChild(noResultsMessage);
+    return;
+  }
+
+  products.forEach((product) => {
+    const card = document.createElement("div");
+    card.classList.add("bg-white", "p-4");
+
+    const name = document.createElement("h2");
+    name.textContent = product.name;
+    name.classList.add("text-lg");
+    card.appendChild(name);
+
+    const price = document.createElement("p");
+    price.textContent = `Price ${product.price}`;
+    card.appendChild(price);
+
+    searchResults.appendChild(card);
+  });
+}
+
+function searchProducts(query) {
+  if (query.length < 3) {
+    searchResults.innerHTML = "";
+    return;
+  }
+
+  const results = product.filter((product) => product.name.toLocaleLowerCase().includes(query.toLowerCase()));
+  displayProducts(results);
+}
+
+searchInput.addEventListener("input", () => {
+  searchProducts(searchInput.value.trim());
+
+});
